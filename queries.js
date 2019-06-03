3d7a1ab4da6dc6560351e7574aed61149516f57b
olehiniakhin <inyakhin.oleh@protonmail.com>
2019-06-03 07:39:01 +0300

    Add createUser function

--- queries.js
+++ queries.js
@@ -30,7 +30,19 @@ const getUserById = (request, response) => {
   })
 }
 
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
 module.exports = {
   getUsers,
   getUserById,
+  createUser,
 }
